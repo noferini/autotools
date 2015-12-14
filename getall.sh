c395ebd8ead5410a9a7811bf0719538a9ffb2e0c
Dario Berzano <dario.berzano@cern.ch>
2015-12-14 16:27:08 +0100

    Bump versions and no rebootstrap on any platform
    * m4 1.4.17
    * libtool 2.4.6
    * autoconf 2.69
    * gettext 0.19.6
    * automake 1.15
    * pkg-config 0.29
    
    Restore timestamps after checkout via `missing-timestamps.sh --apply`, store
    them via `missing-timestamps.sh --store`

---
+++ getall.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+curl -L http://ftp.gnu.org/gnu/m4/m4-1.4.17.tar.gz | tar xzvvf -
+curl -L http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.6.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz | tar xzvvf -
+curl -L http://pkgconfig.freedesktop.org/releases/pkg-config-0.29.tar.gz | tar xzvvf -
