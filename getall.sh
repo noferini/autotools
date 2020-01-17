196908ac73e14a3a946c97c522888a5b4a7a2a0a
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-01-17 15:50:11 +0100

    Move to automake-1.16

--- getall.sh
+++ getall.sh
@@ -3,5 +3,5 @@ curl -L http://ftp.gnu.org/gnu/m4/m4-1.4.17.tar.gz | tar xzvvf -
 curl -L http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz | tar xzvvf -
 curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar xzvvf -
 curl -L http://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.6.tar.gz | tar xzvvf -
-curl -L http://ftp.gnu.org/gnu/automake/automake-1.15.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/automake/automake-1.16.tar.gz | tar xzvvf -
 curl -L http://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz | tar xzvvf -
