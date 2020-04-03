f58dfc22c438adde1552fb21c0fa2052d4a63865
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-04-03 09:49:17 +0200

    Bump packages to get the latest versions
    * automake-1.16 -> automake-1.16.2
    * gettext-0.19.6 -> gettext-0.20.1
    * m4-1.4.17 -> m4-1.4.18

--- getall.sh
+++ getall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
-curl -L http://ftp.gnu.org/gnu/m4/m4-1.4.17.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/m4/m4-1.4.18.tar.gz | tar xzvvf -
 curl -L http://ftpmirror.gnu.org/libtool/libtool-2.4.6.tar.gz | tar xzvvf -
 curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar xzvvf -
-curl -L http://ftp.gnu.org/pub/gnu/gettext/gettext-0.19.6.tar.gz | tar xzvvf -
-curl -L http://ftp.gnu.org/gnu/automake/automake-1.16.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/pub/gnu/gettext/gettext-0.20.1.tar.gz | tar xzvvf -
+curl -L http://ftp.gnu.org/gnu/automake/automake-1.16.2.tar.gz | tar xzvvf -
 curl -L http://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz | tar xzvvf -
