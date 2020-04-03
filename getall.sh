2899648d97c9789bf0e03448011c2e06fff8f3be
Giulio Eulisse <giulio.eulisse@cern.ch>
2020-04-03 13:14:02 +0200

    Add help2man

--- getall.sh
+++ getall.sh
@@ -5,3 +5,4 @@ curl -L http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.gz | tar xzvvf -
 curl -L http://ftp.gnu.org/pub/gnu/gettext/gettext-0.20.1.tar.gz | tar xzvvf -
 curl -L http://ftp.gnu.org/gnu/automake/automake-1.16.2.tar.gz | tar xzvvf -
 curl -L http://pkgconfig.freedesktop.org/releases/pkg-config-0.29.2.tar.gz | tar xzvvf -
+curl -L https://ftp.gnu.org/gnu/help2man/help2man-1.43.2.tar.gz | tar xzvvf -
